af87966e32faa55f5c4d4b98b8c7a08d56705899
Monika Srivastava <monikasrivastava1995@gmail.com>
2023-05-22 16:53:25 +0530

    d13q4

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,25 +1,32 @@
-import './App.css';
+import Header from "./pages/header.js";
 
 function App() {
   return (
     <div>
       <h1>Interpolation Demo</h1>
-      <hr />
-      <InterpolationBasics />
-    </div>
+      <hr /><InterpolationBasics /></div>
   );
 }
+
 function InterpolationBasics() {
-  let title = "INDIA";
+  //1.  Data Member
+  let text = "INDIA";
   let para = `India, officially the Republic of India (ISO: Bhārat Gaṇarājya), is a country in South Asia. 
   It is the seventh-largest country by area, the most populous country as of 1 May 2023, 
   and the most populous democracy in the world.Bounded by the Indian Ocean on the south, 
-  the Arabian Sea on the southwest, and the Bay of Bengal on the southeast`;
+  the Arabian Sea on the southwest, and the Bay of Bengal on the southeast.`;
+
+  let mystyle = { color: "black" };
+
   return (
     <div>
-      <h1>{title}</h1>
-      <p>{para}</p>
+      <h1 style={{ color: "blue" }}>{text}</h1>
+      <div><Header text={Header} style={mystyle} /></div>
+      <p style={{ background: "green", color: "white", padding: "16px" }}>
+        {para}
+      </p>
     </div>
   );
 }
+
 export default App;
---
+++ my-react-project/src/pages/header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <div>
+        <h1 className="bg-primary p-3 text-light">My Country</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
