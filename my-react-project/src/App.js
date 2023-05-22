1a69bbaff41e3d3c52c56517366c45c98f98358e
Monika Srivastava <monikasrivastava1995@gmail.com>
2023-05-22 17:34:56 +0530

    d13q5

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,31 +1,15 @@
-import Header from "./pages/header.js";
+import Header from "./pages/header";
+import BootstrapDemo from "./pages/BootstrapDemo";
+import React from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div>
-      <h1>Interpolation Demo</h1>
-      <hr /><InterpolationBasics /></div>
-  );
-}
-
-function InterpolationBasics() {
-  //1.  Data Member
-  let text = "INDIA";
-  let para = `India, officially the Republic of India (ISO: Bhārat Gaṇarājya), is a country in South Asia. 
+    <div><Header /><BootstrapDemo />India, officially the Republic of India (ISO: Bhārat Gaṇarājya), is a country in South Asia. 
   It is the seventh-largest country by area, the most populous country as of 1 May 2023, 
   and the most populous democracy in the world.Bounded by the Indian Ocean on the south, 
-  the Arabian Sea on the southwest, and the Bay of Bengal on the southeast.`;
-
-  let mystyle = { color: "black" };
-
-  return (
-    <div>
-      <h1 style={{ color: "blue" }}>{text}</h1>
-      <div><Header text={Header} style={mystyle} /></div>
-      <p style={{ background: "green", color: "white", padding: "16px" }}>
-        {para}
-      </p>
-    </div>
+  the Arabian Sea on the southwest, and the Bay of Bengal on the southeast.</div>
   );
 }
 
--- my-react-project/src/pages/BootstrapDemo.js
+++ my-react-project/src/pages/BootstrapDemo.js
@@ -1,7 +1,7 @@
 function BootstrapDemo() {
     return (
       <div>
-        <h1 className="bg-primary p-3 text-light">Bootstrap Demo</h1>
+        <h1 className="bg-primary p-3 text-light">India</h1>
       </div>
     );
   }
