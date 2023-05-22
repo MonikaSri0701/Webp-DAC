a43b510cc1df5e4a7ce7d5d83a36b8859c83e496
Monika Srivastava <monikasrivastava1995@gmail.com>
2023-05-22 16:11:20 +0530

    d13Q3

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
 
 function App() {
-  return <App>Hello World!! Good morning</App>;
+  return (
+    <div>
+      <h1>Interpolation Demo</h1>
+      <hr />
+      <InterpolationBasics />
+    </div>
+  );
 }
-
+function InterpolationBasics() {
+  let title = "INDIA";
+  let para = `India, officially the Republic of India (ISO: Bhārat Gaṇarājya), is a country in South Asia. 
+  It is the seventh-largest country by area, the most populous country as of 1 May 2023, 
+  and the most populous democracy in the world.Bounded by the Indian Ocean on the south, 
+  the Arabian Sea on the southwest, and the Bay of Bengal on the southeast`;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{para}</p>
+    </div>
+  );
+}
 export default App;
