5d198083173d8f35387a6b0b41896df780c4fc76
Monika Srivastava <monikasrivastava1995@gmail.com>
2023-05-23 22:31:41 +0530

    d14q2c

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,54 +1,39 @@
+import React, { useState } from 'react';
 import './App.css';
-import { useState } from "react";
 
 function App() {
+  let [selectedImage] = useState(null);
+  let [activeView, setActiveView] = useState(null);
+
+  let handleButtonClick = (view) => {
+    setActiveView(view);
+  };
+
+  let getViewImage = () => {
+    if (activeView === 'Front View') {
+      return 'https://picsum.photos/id/0/300/300';
+    } else if (activeView === 'Back View') {
+      return 'https://picsum.photos/id/2/300/300';
+    } else if (activeView === 'Side View') {
+      return 'https://picsum.photos/id/6/300/300';
+    } else if (activeView === 'Full View') {
+      return 'https://picsum.photos/id/9/300/300';
+    } else {
+      return selectedImage;
+    }
+  };
+
   return (
-    <div>
-      <EventDemo />
+    <div className="App">
+      <img src={getViewImage()} alt="Selected" />
+      <div>
+        <button onClick={() => handleButtonClick('Front View')}>Front View</button>
+        <button onClick={() => handleButtonClick('Back View')}>Back View</button>
+        <button onClick={() => handleButtonClick('Side View')}>Side View</button>
+        <button onClick={() => handleButtonClick('Full View')}>Full View</button>
+      </div>
     </div>
   );
 }
-function EventDemo() {
-  let [counter, setCounter] = useState(100);
-  let [counter1, setCounter1] = useState(100);
-  let [displayImage, setDisplayImage] = useState(
-    "https://picsum.photos/id/237/300/300"
-  );
-
-  let like = () => {
-    counter = counter + 1;
-    setCounter(counter);
-  };
-
-  let DisLike = () => {
-    counter1 = counter1 + 1;
-    setCounter1(counter1);
-  };
-
-  let changeImage = () => {
-    displayImage = "https://picsum.photos/id/238/300/300";
-    setDisplayImage(displayImage);
-  };
-
-  let resetImage = () => {
-    displayImage = "https://picsum.photos/id/237/300/300";
-    setDisplayImage(displayImage);
-  };
-
-  return (
-    <div>
-      <img src={displayImage} />
-      <h1><span Style="cursor: pointer">&#128077; {counter}</span></h1>
-      <h1><span Style="cursor: pointer">&#128078; {counter1}</span></h1>
-
-
-      <input type="button" value="Like Me" onClick={like} />
-      <input type="button" value="DisLike" onClick={DisLike} />
-      <br />
-      <input type="button" value="Change Image" onClick={changeImage} />
-      <input type="button" value="Reset Image" onClick={resetImage} />
-      </div>
-  );
-  }
 
 export default App;
