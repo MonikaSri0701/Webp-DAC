d7ea9632bb32460d563dddbac32882bf3cf3fe8e
Monika Srivastava <monikasrivastava1995@gmail.com>
2023-05-24 22:42:00 +0530

    updted

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,19 +1,23 @@
-import {useState} from 'react';
-import './App.css';
-import BMIForm from './bmicomponents/BMIForm';
-import BMIScore from './bmicomponents/BMIScore';
+import { useState } from "react";
+import "./App.css";
+import BMIForm from "./bmicomponents/BMIForm";
+import BMISCore from "./bmicomponents/BMIScore";
 
 function App() {
- const [bmi, setBmi] = useState(null);
- const [bmiType] = useState("overweight");
+  const [bmi, setBmi] = useState(18);
+  const [bmiType, setBmiType] = useState("Overweight");
+  const onFormSubmit=(w,h)=>
+  {
+    console.log(w+":::"+h);
+  }
+
   return (
     <div className="App">
-      <h2>BMI CALCULATOR</h2>
-      <button onClick={() => setBmi(bmi+5)}>Click</button>
-      <BMIForm/>
-      <BMIScore Mybmi = {bmi} Type = {bmiType}/>
-
+      <h2>Welcome to REACT</h2>
+      <BMIForm getData={onFormSubmit}/>
+      <BMISCore />
     </div>
   );
 }
+
 export default App;
--- my-react-project/src/bmicomponents/BMIForm.js
+++ my-react-project/src/bmicomponents/BMIForm.js
@@ -1,32 +1,49 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-function BMIForm() {
-  const [Height, setHeight] = useState("");
-  const [Weight, setWeight] = useState("");
-  const onSubmit = (e) => e.preventDefault();
-  if(isNaN(Weight) || isNaN(Height))
-  {
-    console.log("wrong input");
-  }
-  else
-  getData
+function BMIForm({getData}) {
+    const [height,setHeight] = useState("");
+    const [weight,setWeight] = useState("");
+const onSubmit = (e)=>
+ e.preventDefault();
+if(isNaN(weight)||isNaN(height))
+{
+    console.log("Wrong input");
+}
+else
+getData(weight,height)
+
+
   return (
     <div>
-      <Form classname ="formbmi"
-    <Form.Group className="FormInput" controlId="formHeight">
+      BMIForm for user input
+      <Form className="formbmi" onSubmit={onSubmit}>
+        <Form.Group className="FormInput" controlId="formHeight">
           <Form.Control 
           type="text"
            placeholder="Height in M" 
-           value={Height}
+           value={height}
            onChange={(e)=>setHeight(e.target.value)}
            className="form-control"
            />
-        <Form.Control type="text" placeholder="Height(in mts)" />
         </Form.Group>
-        <Button variant="primary" type="submit">Get BMI</Button>
-      </div>
+        <Form.Group className="FormInput" controlId="formWeight">
+          <Form.Control 
+          type="text" 
+          placeholder="Weight in Kg" 
+          value={weight}
+          onChange={(e)=>setWeight(e.target.value)}
+          className="form-control"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Get BMI
+        </Button>
+      </Form>
+    </div>
+       
   );
 }
+
 export default BMIForm;
